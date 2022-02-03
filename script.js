d5d0a7e5e3a70945bb9a5a06f454befc475f7d92
marcellpret <marcellpret@gmail.com>
2022-02-04 00:39:29 +0100

    Checking matches and reloading

--- script.js
+++ script.js
@@ -51,12 +51,14 @@ const deck = [
     },
 ];
 
-// console.log("deck: ", deck);
+const shuffledDeck = deck.sort((a, b) => 0.5 - Math.random());
+
+console.log("shuffledDeck: ", shuffledDeck);
 
 const layDeck = () => {
     let allTheCards = "";
 
-    deck.forEach((card) => {
+    shuffledDeck.forEach((card) => {
         console.log("card: ", card);
         allTheCards += `<div class="card ${card.name}" >
         <img class="cardImg" src=${card.url} alt=${card.name}>
@@ -73,8 +75,22 @@ layDeck();
 const cards = document.querySelectorAll(".card");
 const cover = document.querySelectorAll(".cover");
 
+// const reset = () => {
+//     let cardsMatched = 0;
+//     console.log("Estou rodando");
+
+//     cards.forEach((card) => {
+//         if (card.classList.contains("matched")) {
+//             cardsMatched += 1;
+//             console.log("cardsMatched: ", cardsMatched);
+//         }
+//     });
+// };
+
+// reset();
+
 // console.log("cards: ", cards);
-// console.log("cover: ", cover);
+console.log("cover: ", cover);
 
 cover.forEach((cover, i) => {
     cover.addEventListener("click", function (e) {
@@ -86,6 +102,7 @@ cover.forEach((cover, i) => {
     });
 });
 
+let cardsMatched = 0;
 const checkEqualCards = () => {
     let flips = 0;
     let flippedCards = [];
@@ -94,11 +111,13 @@ const checkEqualCards = () => {
             flips += 1;
         }
     });
+
     if (flips === 2) {
         console.log("we got 2 cards");
         deck.forEach((card) => {
             if (card.isFlipped) {
                 flippedCards.push(card);
+                console.log("card: ", card);
             }
         });
 
@@ -109,9 +128,29 @@ const checkEqualCards = () => {
                     console.log("card: ", card);
                     setTimeout(() => {
                         card.classList.add("matched");
+                        if (card.classList.contains("matched")) {
+                            cardsMatched += 1;
+                            if (cardsMatched === cards.length) {
+                                location.reload();
+                            }
+                        }
+                        deck.forEach((card) => (card.isFlipped = false));
+                        flips = 0;
+                        flippedCards = [];
                     }, 1500);
                 }
             });
+        } else {
+            setTimeout(() => {
+                cover.forEach((card) => {
+                    card.classList.remove("flip");
+                });
+                deck.forEach((card) => (card.isFlipped = false));
+                flips = 0;
+                flippedCards = [];
+                console.log("deck: ", deck);
+                console.log("flippedCards: ", flippedCards);
+            }, 1500);
         }
     }
 };
